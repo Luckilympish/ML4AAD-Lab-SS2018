20102368880bd18427785d2d9adf3322977b66d7
Gresa Shala <shalag@informatik.uni-freiburg.de>
2018-05-30 18:19:04 +0200

    added validation of LPG optimization with SMAC

--- Exercise2/HPO_deep_neural_net/dn_net/KerasWrapper.py
+++ Exercise2/HPO_deep_neural_net/dn_net/KerasWrapper.py
@@ -2,7 +2,10 @@ import sys
 import re
 import math
 import logging
-
+import numpy
+import keras
+import tensorflow
+from sklearn.model_selection import train_test_split
 from abstractBlackBoxWrapper import AbstractBlackBoxWrapper
 
 class KerasWrapper(AbstractBlackBoxWrapper):
@@ -13,8 +16,9 @@ class KerasWrapper(AbstractBlackBoxWrapper):
         
     def get_value(self, instance, config):
         return self._get_accuracy_value(instance, config)
-    def read_instance(instance):
-	X = instance.split("\n")
+    def read_instance(self, instance):
+        dataset = numpy.loadtxt(instance, delimiter=",")
+        X = dataset.split("\n")
         X1 = []
         X3 = []
         for row in X:
@@ -27,15 +31,16 @@ class KerasWrapper(AbstractBlackBoxWrapper):
         return X3
 
     def _get_accuracy_value(self, instance, cfg):
-	from numpy.random import seed
-        dataset = self.read_instance(instance)
+        numpy.random.seed(1)
+        tensorflow.set_random_seed(2)
+        dataset = numpy.loadtxt(instance, delimiter=",")
         X = dataset[:,0:8]
         Y = dataset[:,8]
         X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.2)
 	# create model
-        model = Sequential()
-        model.add(Dense(cfg["neurons"], input_dim=8, kernel_initializer=cfg["kernel_initializer"], activation=cfg["activation"]))
-        model.add(Dense(1, activation='sigmoid'))
+        model = keras.models.Sequential()
+        model.add(keras.layers.Dense(cfg["neurons"], input_dim=8, kernel_initializer=cfg["kernel_initializer"], activation=cfg["activation"]))
+        model.add(keras.layers.Dense(1, activation='sigmoid'))
         # Compile model
         model.compile(loss='binary_crossentropy', optimizer=cfg["optimizer"], metrics=['accuracy'])
         model.fit(X_train, y_train, batch_size=cfg["batch_size"], epochs=cfg["epochs"])
---
+++ Exercise2/lpg_depots/validate/plot_validation_1.py
@@ -0,0 +1,42 @@
+import os
+from smac.scenario.scenario import Scenario
+from smac.runhistory.runhistory import RunHistory
+import numpy
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+"""
+This file runs SMAC and then restores the run with an extended computation
+budget. This will also work for SMAC runs that have crashed and are continued.
+"""
+
+def main():
+    scenario = Scenario("scenario.txt")
+    # We load the runhistory, ...
+    rh_path = os.path.join("validated_runhistory.json")
+    runhistory = RunHistory(aggregate_func=None)
+    runhistory.load_json(rh_path, scenario.cs)
+    
+    cost_default = []
+    cost_incumbent = []
+    for entry,values in runhistory.data.items():  # iterate over data because it is an OrderedDict
+        config_id = entry.config_id  # look up config id
+        config = runhistory.ids_config[config_id]  # look up config
+        z_ = values.cost  # get cost
+        if config_id == 1:
+            cost_default.append(z_)
+        else:
+            cost_incumbent.append(z_)
+    plt.plot(cost_incumbent,cost_default)
+
+    x = numpy.linspace(0,max(max(cost_incumbent),max(cost_default)),0.1)
+    plt.plot(x,x,'k')
+    plt.ylabel("Default Confniguration")
+    plt.xlabel("Incumbent of run 1")
+    plt.title("Performance of Incumbents compared to Default Configuration")
+    
+    plt.savefig("perf_inc_vs_def.png")
+if "__main__" == __name__:
+    
+    main()
