d9d02e9e4d42e6f4b00ee7ec4535237306708c96
Gresa Shala <shalag@informatik.uni-freiburg.de>
2018-06-04 15:36:14 +0200

    Added modification

--- Exercise2/HPO_deep_neural_net/dn_net/KerasWrapper.py
+++ Exercise2/HPO_deep_neural_net/dn_net/KerasWrapper.py
@@ -4,9 +4,13 @@ import math
 import logging
 import numpy
 import keras
+from keras import optimizers
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 import tensorflow
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from abstractBlackBoxWrapper import AbstractBlackBoxWrapper
 
 class KerasWrapper(AbstractBlackBoxWrapper):
@@ -17,38 +21,68 @@ class KerasWrapper(AbstractBlackBoxWrapper):
         
     def get_value(self, instance, config):
         return self._get_accuracy_value(instance, config)
-    def read_instance(self, instance):
-        dataset = numpy.loadtxt(instance, delimiter=",")
-        X = dataset.split("\n")
-        X1 = []
-        X3 = []
-        for row in X:
-            X1.append(row.split(","))
-        for row in range(len(X1)):
-            X2 = []
-            for item in range(len(X1[row])):
-                X2.append(float(X1[row][item]))
-            X3.append(X2)
-        return X3
 
     def _get_accuracy_value(self, instance, cfg):
         numpy.random.seed(1)
         tensorflow.set_random_seed(2)
-        dataset = numpy.loadtxt(instance, delimiter=",")
-        X = dataset[:,0:8]
-        Y = dataset[:,8]
-        X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.2)
-	# create model
-        model = keras.models.Sequential()
-        model.add(keras.layers.Dense(int(cfg["neurons"]), input_dim=8, kernel_initializer=cfg["kernel_initializer"], activation=cfg["activation"]))
-        print(dataset)
-        model.add(keras.layers.Dense(1, activation='sigmoid'))
-        # Compile model
-        model.compile(loss='binary_crossentropy', optimizer=cfg["optimizer"], metrics=['accuracy'])
-        model.fit(X_train, y_train, batch_size=int(cfg["batch_size"]), epochs=int(cfg["epochs"]))
-        y_pred = model.predict(X_test)
-        y_pred = (y_pred > 0.5)
-        return accuracy_score(y_test, y_pred)
+        num_classes = 10
+        img_rows, img_cols = 28, 28
+        databounds = numpy.loadtxt(instance, delimiter=",", dtype='int')
+        train_index = databounds[0]
+        test_index = databounds[1]
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        if K.image_data_format() == 'channels_first':
+            x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+            x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+            input_shape = (1, img_rows, img_cols)
+        else:
+            x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+            x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+            input_shape = (img_rows, img_cols, 1)
+
+        x_train = x_train.astype('float32')
+        x_test = x_test.astype('float32')
+        x_train /= 255
+        x_train=x_train[train_index[0]:train_index[1],:,:,:]
+        x_test /= 255
+        x_test=x_test[test_index[0]:test_index[1],:,:,:]
+	# convert class vectors to binary class matrices
+        y_train = keras.utils.to_categorical(y_train[train_index[0]:train_index[1]], num_classes)
+        y_test = keras.utils.to_categorical(y_test[test_index[0]:test_index[1]], num_classes)
+
+        model = Sequential()
+        model.add(Conv2D(int(cfg["neurons_1"]), kernel_size=(3, 3),activation=cfg["activation_1"],input_shape=input_shape))
+        model.add(Conv2D(int(cfg["neurons_2"]), (3, 3), activation=cfg["activation_2"]))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(float(cfg["dropout_1"])))
+        model.add(Flatten())
+        model.add(Dense(int(cfg["neurons_3"]), activation=cfg["activation_3"]))
+        model.add(Dropout(float(cfg["dropout_2"])))
+        model.add(Dense(num_classes, activation=cfg["activation_4"]))
+
+        optimizer_str = cfg["optimizer"]
+        if optimizer_str == 'SGD':
+            nesterov = False
+            if cfg["opt_nesterov"] == 'true':
+                nesterov = True
+            optimizer = optimizers.SGD(lr=float(cfg["opt_lr"]), momentum=float(cfg["opt_momentum"]), decay=float(cfg["opt_decay"]), nesterov=nesterov)
+        elif optimizer_str == 'RMSprop':
+            optimizer = optimizers.RMSprop(lr=float(cfg["opt_lr"]), rho=float(cfg["opt_rho"]), decay=float(cfg["opt_decay"]))
+        elif optimizer_str == 'Adagrad':
+            optimizer = optimizers.Adagrad(lr=float(cfg["opt_lr"]), decay=float(cfg["opt_decay"]))
+        elif optimizer_str == 'Adadelta':
+            optimizer = optimizers.Adadelta(lr=float(cfg["opt_lr"]), rho=float(cfg["opt_rho"]), decay=float(cfg["opt_decay"]))
+        elif optimizer_str == 'Adam':
+            optimizer = optimizers.Adam(lr=float(cfg["opt_lr"]), beta_1=float(cfg["opt_beta1"]), beta_2=float(cfg["opt_beta2"]), decay=float(cfg["opt_decay"]))
+        elif optimizer_str == 'Adamax':
+            optimizer = optimizers.Adamax(lr=float(cfg["opt_lr"]), beta_1=float(cfg["opt_beta1"]), beta_2=float(cfg["opt_beta2"]), decay=float(cfg["opt_decay"]))
+        else :
+            optimizer = optimizers.Nadam(lr=float(cfg["opt_lr"]), beta_1=float(cfg["opt_beta1"]), beta_2=float(cfg["opt_beta2"]))
+        model.compile(loss=keras.losses.categorical_crossentropy, optimizer=optimizer, metrics=['accuracy'])
+
+        model.fit(x_train, y_train, batch_size=int(cfg["batch_size"]), epochs=int(cfg["epochs"]), verbose=1, validation_data=(x_test, y_test))
+        score = model.evaluate(x_test, y_test, verbose=0)
+        return score[1]
 
 if __name__ == "__main__":
     wrapper = KerasWrapper()
---
+++ Exercise2/lpg_depots/plot_validation.py
@@ -0,0 +1,54 @@
+import os
+from smac.scenario.scenario import Scenario
+from smac.runhistory.runhistory import RunHistory
+import numpy
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='test', fromfile_prefix_chars="@")
+    parser.add_argument('-s', '--scenario_file', dest='scenario', required=True)
+    parser.add_argument('-rh', '--runhistory_file', dest='runhistory', required=True)
+    parser.add_argument('-o', '--output_file', dest='output', required=True)
+    args = parser.parse_args()
+
+
+    scenario = Scenario(args.scenario)
+    # We load the runhistory, ...
+    rh_path = os.path.join(args.runhistory)
+    runhistory = RunHistory(aggregate_func=None)
+    runhistory.load_json(rh_path, scenario.cs)
+    
+    cost_default = []
+    cost_incumbent = []
+    for entry,values in runhistory.data.items():  # iterate over data because it is an OrderedDict
+        config_id = entry.config_id  # look up config id
+        config = runhistory.ids_config[config_id]  # look up config
+        z_ = values.cost  # get cost
+        if z_ > 100:
+            z_ = 150
+        if config_id == 1:
+            cost_default.append(z_)
+        else:
+            cost_incumbent.append(z_)
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+    ax1.plot(cost_incumbent, cost_default, linestyle='None', marker='o', color="black")
+    ax1.plot([0,100],[0,100],'r-')
+    ax1.plot([0,100],[100,100], linestyle='dashed', color="black")
+    ax1.plot([100,100],[100,0],linestyle='dashed', color="black")
+    plt.ylabel("Default Configuration")
+    plt.xlabel("Incumbent")
+    ax1.set_xlim([0,175])
+    ax1.set_ylim([0,175])
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.title("Performance of Incumbent compared to Default Configuration")
+    plt.savefig(args.output)
+
+
+if "__main__" == __name__:
+    
+    main()
