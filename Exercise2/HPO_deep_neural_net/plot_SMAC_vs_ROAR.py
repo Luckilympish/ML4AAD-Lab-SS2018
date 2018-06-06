c4afbdd645aeff4b9ef3cee4b164d5e5f8bbe926
Gresa Shala <shalag@informatik.uni-freiburg.de>
2018-06-06 23:28:36 +0200

    Added plots

--- Exercise2/HPO_deep_neural_net/plot_SMAC_vs_ROAR.py
+++ Exercise2/HPO_deep_neural_net/plot_SMAC_vs_ROAR.py
@@ -8,19 +8,6 @@ import matplotlib.pyplot as plt
 import argparse
 
 
-def med_and_percentiles(y):
-    y_min = numpy.zeros(len(y))
-    y_median = numpy.zeros(len(y))
-    y_25th = numpy.zeros(len(y))
-    y_75th = numpy.zeros(len(y))
-    for id in range(len(y)):
-        y_min[id] = numpy.min(y[:id+1])
-        y_median[id] = numpy.percentile(y_min[:id+1], 50)
-        y_25th[id] = numpy.percentile(y_min[:id+1], 25)
-        y_75th[id] = numpy.percentile(y_min[:id+1], 75)
-    return y_median, y_25th, y_75th
-           
-
 def main():
     parser = argparse.ArgumentParser(description='test', fromfile_prefix_chars="@")
     parser.add_argument('-s', '--scenario_SMAC', dest='scenario_SMAC', required=True)
@@ -56,19 +43,13 @@ def main():
         config = runhistory_ROAR.ids_config[config_id]  # look up config
         z_ = values.cost  # get cost
         cost_ROAR.append(z_)
-    smac_med, smac_25th, smac_75th = med_and_percentiles(cost_SMAC)
-    roar_med, roar_25th, roar_75th = med_and_percentiles(cost_ROAR)
+
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111)
     x = range(20) #comparison is done only for 20 runs
-    #plot SMAC performance
-    ax1.plot(smac_med, 'o', color='red')
-    ax1.step(smac_med, 'k', where="post", label='SMAC', color='red')
-    plt.fill_between(x, smac_25th, smac_75th, alpha=0.5, color='red', step='pre')
-    #plot ROAR performance
-    #ax1.plot(roar_med, 'o', color='blue')
-    #ax1.step(roar_med, 'k', where="post", label='ROAR', color='blue')
-    #plt.fill_between(x, roar_25th, roar_75th, alpha=0.5, color='blue', step='pre')
+    #plot SMAC and ROAR performance
+    ax1.plot(cost_SMAC, 'o', color='red',label='SMAC')
+    ax1.plot(cost_ROAR, '+', color='blue',label='ROAR')
 
     plt.ylabel("Loss")
     plt.xlabel("#Evaluations")
